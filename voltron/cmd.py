b2b1b4385d17ec03b09ac3e98dc17de1e42da3bd
snarez <snarez+github@ragequ.it>
2013-07-08 23:15:51 +1000

    Refactored everything, split into files, added setup.py

---
+++ voltron/cmd.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+
+import logging
+
+from comms import *
+
+DISASM_MAX = 32
+STACK_MAX = 64
+
+log = logging.getLogger('voltron')
+
+class VoltronCommand (object):
+    running = False
+
+    def handle_command(self, command):
+        global log
+        if "start" in command:
+            if 'debug' in command:
+                log.setLevel(logging.DEBUG)
+            self.start()
+        elif "stop" in command:
+            self.stop()
+        elif "status" in command:
+            self.status()
+        elif "update" in command:
+            self.update()
+        else:
+            print("Usage: voltron <start|stop|update|status>")
+
+    def start(self):
+        if not self.running:
+            print("Starting voltron")
+            self.running = True
+            self.register_hooks()
+            self.server = Server()
+            self.server.start()
+        else:
+            print("Already running")
+
+    def stop(self):
+        if self.running:
+            print("Stopping voltron")
+            self.unregister_hooks()
+            self.server.stop()
+            self.server = None
+            self.running = False
+        else:
+            print("Not running")
+
+    def status(self):
+        if self.running:
+            print("There are {} clients attached".format(len(clients)))
+            for client in clients:
+                print("{} registered with config: {}".format(client, str(client.registration['config'])))
+        else:
+            print("Not running")
+
+    def update(self):
+        log.debug("Updating clients")
+
+        for client in filter(lambda c: c.registration['config']['update_on'] == 'stop', clients):
+            event = {'msg_type': 'update', }
+
+            if client.registration['config']['type'] == 'cmd':
+                event['data'] = self.get_cmd_output(client.registration['config']['cmd'])
+            elif client.registration['config']['type'] == 'register':
+                event['data'] = self.get_registers()
+            elif client.registration['config']['type'] == 'disasm':
+                event['data'] = self.get_disasm()
+            elif client.registration['config']['type'] == 'stack':
+                event['data'] = {'data': self.get_stack(), 'sp': self.get_register('rsp')}
+            elif client.registration['config']['type'] == 'bt':
+                event['data'] = self.get_backtrace()
+                
+            self.server.enqueue_event(client, event)
+
+    def register_hooks(self):
+        pass
+
+    def unregister_hooks(self):
+        pass
---
+++ voltron/gdbcmd.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+
+import os
+import sys
+import gdb
+import logging
+
+from voltron.cmd import *
+
+log = logging.getLogger('voltron')
+
+class VoltronGDBCommand (VoltronCommand, gdb.Command):
+    def __init__(self):
+        super(VoltronCommand, self).__init__("voltron", gdb.COMMAND_NONE, gdb.COMPLETE_NONE)
+        self.running = False
+
+    def invoke(self, arg, from_tty):
+        self.handle_command(arg)
+
+    def register_hooks(self):
+        gdb.events.stop.connect(self.stop_handler)
+
+    def unregister_hooks(self):
+        gdb.events.stop.disconnect(self.stop_handler)
+
+    def stop_handler(self, event):
+        self.update()
+
+    def get_registers(self):
+        log.debug('Getting registers')
+        regs = ['rax','rbx','rcx','rdx','rbp','rsp','rdi','rsi','rip','r8','r9','r10','r11','r12','r13','r14','r15',
+                'cs','ds','es','fs','gs','ss','xmm0','xmm1','xmm2','xmm3','xmm4','xmm5','xmm6','xmm7','xmm8',
+                'xmm9','xmm10','xmm11','xmm12','xmm13','xmm14','xmm15']
+        vals = {}
+        for reg in regs:
+            try:
+                vals[reg] = int(gdb.parse_and_eval('(long long)$'+reg)) & 0xFFFFFFFFFFFFFFFF
+            except:
+                log.debug('Failed getting reg: ' + reg)
+                vals[reg] = 'N/A'
+        try:
+            vals['rflags'] = int(gdb.execute('info reg $eflags', to_string=True).split()[1], 16)
+        except:
+            log.debug('Failed getting reg: eflags')
+            vals['rflags'] = 'N/A'
+        log.debug('Got registers: ' + str(vals))
+        return vals
+
+    def get_register(self, reg):
+        log.debug('Getting register: ' + reg)
+        return int(gdb.parse_and_eval('(long long)$'+reg)) & 0xFFFFFFFFFFFFFFFF
+
+    def get_disasm(self):
+        log.debug('Getting disasm')
+        res = gdb.execute('x/{}i $rip'.format(DISASM_MAX), to_string=True)
+        return res
+
+    def get_stack(self):
+        log.debug('Getting stack')
+        rsp = int(gdb.parse_and_eval('(long long)$rsp')) & 0xFFFFFFFFFFFFFFFF
+        res = str(gdb.selected_inferior().read_memory(rsp, STACK_MAX*16))
+        return res
+
+    def get_backtrace(self):
+        log.debug('Getting backtrace')
+        res = gdb.execute('bt', to_string=True)
+        return res
+
+    def get_cmd_output(self, cmd=None):
+        if cmd:
+            log.debug('Getting command output: ' + cmd)
+            res = gdb.execute(cmd, to_string=True)
+        else:
+            res = "<No command>"
+        return res
+
+
+if __name__ == "__main__":
+    log.debug('Loading GDB command')
+    print("Voltron loaded.")
+    inst = VoltronGDBCommand()
+
