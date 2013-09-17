e4369a6952acfdce8e3599356517070fdf87e0a0
snare <snare@ho.ax>
2013-09-17 19:21:35 +1000

    Don't commit when drunk

--- setup.py
+++ setup.py
@@ -11,8 +11,9 @@ setup(
     url = "https://github.com/snarez/voltron",
     packages=['voltron'],
     install_requires = [],
-    data_files=['voltron.gdb', 'voltron.cfg'],
+    data_files=['voltron.gdb', 'voltron.cfg', 'dbgentry.py'],
     entry_points = {
         'console_scripts': ['voltron = voltron:main']
-    }
+    },
+    zip_safe = False
 )
