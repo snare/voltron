20bcf1ae40932a1079f9f17101ab7c92e89dd619
snare <snare@ho.ax>
2014-12-20 14:41:31 +1100

    Cleaned up plugin initialisation

--- voltron/plugin.py
+++ voltron/plugin.py
@@ -57,17 +57,14 @@ class PluginManager(object):
         This is called by __init__(), but may also be called by the debugger
         host to load a specific plugin at runtime.
         """
+        if hasattr(plugin, 'initialise'):
+            plugin.initialise()
         if self.valid_api_plugin(plugin):
             log.debug("Registering API plugin: {}".format(plugin))
-            p = plugin()
-            p.request_class._plugin = plugin
-            p.request_class.request = plugin.request
-            self._api_plugins[plugin.request] = p
+            self._api_plugins[plugin.request] = plugin()
         elif self.valid_debugger_plugin(plugin):
             log.debug("Registering debugger plugin: {}".format(plugin))
-            p = plugin()
-            p.adaptor_class._plugin = plugin
-            self._debugger_plugins[plugin.host] = p
+            self._debugger_plugins[plugin.host] = plugin()
         elif self.valid_view_plugin(plugin):
             log.debug("Registering view plugin: {}".format(plugin))
             self._view_plugins[plugin.name] = plugin()
@@ -157,7 +154,13 @@ class PluginManager(object):
         return self.web_plugins[host]
 
 
-class APIPlugin(Plugin):
+class VoltronPlugin(Plugin):
+    @classmethod
+    def initialise(cls):
+        pass
+
+
+class APIPlugin(VoltronPlugin):
     """
     Abstract API plugin class. API plugins subclass this.
 
@@ -183,7 +186,14 @@ class APIPlugin(Plugin):
     response_class = None
     supported_hosts = ['core']
 
-class DebuggerAdaptorPlugin(Plugin):
+    @classmethod
+    def initialise(cls):
+        if cls.request_class:
+            cls.request_class._plugin = cls
+            cls.request_class.request = cls.request
+
+
+class DebuggerAdaptorPlugin(VoltronPlugin):
     """
     Debugger adaptor plugin parent class.
 
@@ -197,7 +207,13 @@ class DebuggerAdaptorPlugin(Plugin):
     adaptor_class = None
     supported_hosts = ['core']
 
-class ViewPlugin(Plugin):
+    @classmethod
+    def initialise(cls):
+        if cls.adaptor_class:
+            cls.adaptor_class._plugin = cls
+
+
+class ViewPlugin(VoltronPlugin):
     """
     View plugin parent class.
 
@@ -211,7 +227,14 @@ class ViewPlugin(Plugin):
     name = None
     view_class = None
 
-class WebPlugin(Plugin):
+    @classmethod
+    def initialise(cls):
+        if cls.view_class:
+            cls.view_class._plugin = cls
+            cls.view_class.view_type = cls.name
+
+
+class WebPlugin(VoltronPlugin):
     """
     Web plugin parent class.
 
