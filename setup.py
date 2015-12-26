bee1f1bf1c8eafda3d910990b3705b94e4658282
snare <snare@ho.ax>
2015-12-26 16:50:55 +1100

    Add requests_unixsocket dep

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "voltron",
-    version = "0.1",
-    author = "snare",
-    author_email = "snare@ho.ax",
-    description = ("A UI for GDB & LLDB"),
-    license = "Buy snare a beer",
-    keywords = "voltron gdb lldb",
-    url = "https://github.com/snarez/voltron",
+    name="voltron",
+    version="0.1",
+    author="snare",
+    author_email="snare@ho.ax",
+    description=("A UI for GDB & LLDB"),
+    license="Buy snare a beer",
+    keywords="voltron gdb lldb",
+    url="https://github.com/snarez/voltron",
     packages=find_packages(),
-    install_requires = ['rl', 'scruffington', 'flask', 'blessed', 'pygments'],
+    install_requires=['rl', 'scruffington', 'flask', 'blessed', 'pygments', 'requests_unixsocket'],
     data_files=['dbgentry.py'],
-    package_data = {'voltron': ['config/*']},
-    install_package_data = True,
-    entry_points = {
-        'console_scripts': ['voltron = voltron:main']
+    package_data={'voltron': ['config/*']},
+    install_package_data=True,
+    entry_points={
+        'console_scripts': ['voltron=voltron:main']
     },
-    zip_safe = False
+    zip_safe=False
 )
