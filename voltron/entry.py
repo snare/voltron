bcefffe4fecf7da7670797f5bc13b26c9e715ad5
snare <snare@ho.ax>
2016-04-12 17:53:18 +1000

    Clean up path fix

--- voltron/entry.py
+++ voltron/entry.py
@@ -24,23 +24,14 @@ WinDbg/CDB (via PyKD):
 log = None
 
 try:
+    # fix path if it's clobbered by brew
     import sys
     if sys.platform == 'darwin':
-        sys.path = [p for p in sys.path if 'Cellar' not in p]
-
-        new_path = ['/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python27.zip',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-darwin',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac/lib-scriptpackages',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-old',
-                    '/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload']
-
-        for p in new_path:
-            if p not in sys.path:
-                sys.path.append(p)
+        py_base = '/System/Library/Frameworks/Python.framework/Versions/2.7/'
+        new_path = ['lib/python27.zip', 'lib/python2.7', 'lib/python2.7/plat-darwin', 'lib/python2.7/plat-mac',
+                    'lib/python2.7/plat-mac/lib-scriptpackages', 'Extras/lib/python', 'lib/python2.7/lib-tk',
+                    'lib/python2.7/lib-old', 'lib/python2.7/lib-dynload']
+        sys.path = [p for p in sys.path if 'Cellar' not in p] + [py_base + p for p in new_path]
 except:
     pass
 
