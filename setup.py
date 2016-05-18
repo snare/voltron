e1e76c86e24d0f609b5a7f04ced3b59af3caf98a
snare <snare@ho.ax>
2016-05-18 15:18:01 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.platform == 'win32':
 
 setup(
     name="voltron",
-    version="0.1.4",
+    version="0.1.5",
     author="snare",
     author_email="snare@ho.ax",
     description=("A debugger UI"),
