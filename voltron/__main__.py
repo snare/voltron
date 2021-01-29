e82bf244fa5c8ea292d96683a822523138c61641
Zachary Cutlip <uid000@gmail.com>
2021-01-29 12:20:42 -0800

    fix ImportError when debugging voltron in VSCode

--- voltron/__main__.py
+++ voltron/__main__.py
@@ -1,3 +1,10 @@
-from .main import main
+try:
+    # for some reason the relative import doesn't work in VSCode's
+    # interactive debugger, but this does but I'm not sure if there's a
+    # chance a different voltron could be somewhere in sys.path, so
+    # let's try the relative import first
+    from .main import main
+except ImportError:
+    from voltron.main import main
 
 main()
